31bc45642c61384ae5380689b9560f01dcb96e7b
David Cannady <David Cannady>
2016-04-05 17:28:19 -0700

    surveyForm

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test Show</title>
+  </head>
+  <body>
+    <fieldset>
+      <div id='Dimensions'>
+        <h3>Submitted Information</h3>
+        <p>First Name: <?php echo $_POST['first_name'];?></p>
+        <p>Last Name: <?php echo $_POST['last_name'];?></p>
+        <p>
+            <!-- The difference between Get and Post is Post
+          processes information and sends it to a web page.
+        Get only displays a web page. -->
+    This is the best day.
+
+  </body>
+</html>
---
+++ surveyForm.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <div id='Dimensions'>
+      <form action="main.php" method="post">
+      <!-- <input type="text" name="first_name">
+      <input type="text" name="last_name"> -->
+      <p>First Name<input type="text"name="first_name"></p>
+      <p>Last Name<input type="text" name="last_name"></p>
+      <p>Dojo Location:
+        <select name='location'>
+          <option value='Portland, OR'>Portland, OR</option>
+          <option value='Seattle, WA'>Seattle, WA</option>
+          <option value='Chicago, IL'>Chicago, IL</option>
+          <option value='Dallas, TX'>Dallas, TX</option>
+        </select></p>
+      <p>Favorite Language
+        <select name='language'>
+          <option value='Rail'>Rail</option>
+          <option value='Ruby'>Ruby</option>
+          <option value='Python'>Python</option>
+          <option value='PHP'>PHP</option>
+        </select></p>
+      <input type="submit"> <value type="submit">
+
+
+      </form>
+
+
+  </body>
+</html>
